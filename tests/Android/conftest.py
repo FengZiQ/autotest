6a544c4380b746f6329e7fa35732875675b24a08
fzq <feng1025352529@qq.com>
2025-11-28 19:32:12 +0800

    测试报告输出断言图片

--- tests/Android/conftest.py
+++ tests/Android/conftest.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
 import time
+import logging
 import pytest
 from pytest_html import extras
-from utils.base64_util import image_html_content
 from appium.webdriver.webdriver import AppiumBy
 from config.android_config import test_app_config
 from core.android_client import AndroidAutomationTool
@@ -12,7 +12,7 @@ from core.android_test_executor import AndroidTestExecutor
 
 # 网易阅读app
 @pytest.fixture(scope="session")
-def netease_pris():
+def appium_server():
     # 创建工具实例
     automation_tool = AndroidAutomationTool()
     try:
@@ -20,24 +20,32 @@ def netease_pris():
         automation_tool.setup_driver(test_app_config)
         time.sleep(3)
     except Exception as e:
-        logger.warning(f"驱动启动失败，退出测试! 错误信息：{e}")
+        logging.warning(f"驱动启动失败，退出测试! 错误信息：{e}")
         pytest.exit('驱动启动失败，退出测试!')
+
+    yield automation_tool
+
+    automation_tool.quit_driver()
+
+
+@pytest.fixture(scope="function")
+def netease_pris(appium_server):
     is_debug = True
     if not is_debug:
         # 启动测试app
-        automation_tool.start_app(
+        appium_server.start_app(
             app_package='com.netease.pris',
             app_activity='.activity.PRISActivityFlasScreen'
         )
 
         # 登录
-        automation_tool.click_element(
+        appium_server.click_element(
             locator=(
                 AppiumBy.XPATH,
                 '//android.widget.TextView[@resource-id="com.netease.pris:id/title" and @text="我的"]'
             )
         )
-        login_button = automation_tool.find_element(
+        login_button = appium_server.find_element(
             locator=(
                 AppiumBy.ID,
                 'com.netease.pris:id/button_login'
@@ -45,27 +53,27 @@ def netease_pris():
             timeout=2
         )
         if login_button:
-            automation_tool.click_element(
+            appium_server.click_element(
                 locator=(
                     AppiumBy.ID,
                     'com.netease.pris:id/button_login'
                 )
             )
-            automation_tool.send_keys(
+            appium_server.send_keys(
                 locator=(
                     AppiumBy.ID,
                     'com.netease.pris:id/ui_login_name'
                 ),
                 text='13717641870'
             )
-            automation_tool.send_keys(
+            appium_server.send_keys(
                 locator=(
                     AppiumBy.ID,
                     'com.netease.pris:id/ui_login_password'
                 ),
                 text='tester'
             )
-            automation_tool.click_element(
+            appium_server.click_element(
                 locator=(
                     AppiumBy.ID,
                     'com.netease.pris:id/ui_login_button_login'
@@ -73,12 +81,12 @@ def netease_pris():
             )
 
     # 创建用例自动化执行器
-    executor = AndroidTestExecutor(automation_tool)
+    executor = AndroidTestExecutor(appium_server)
 
     yield executor
 
-    # 退出驱动
-    executor.android_tool.quit_driver()
+    # 重置一下step_screenshots
+    executor.step_screenshots = []
 
 
 @pytest.hookimpl(hookwrapper=True)
